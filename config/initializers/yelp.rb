b8d4cc20452ae29b4738b2f078a75d9e1b91254d
iregina <reginawong14@gmail.com>
2016-03-18 19:53:16 -0700

    add yelp ruby gem

---
+++ config/initializers/yelp.rb
@@ -0,0 +1,7 @@
+# require 'yelp'
+
+# client = Yelp::Client.new({ consumer_key: YOUR_CONSUMER_KEY,
+#                             consumer_secret: YOUR_CONSUMER_SECRET,
+#                             token: YOUR_TOKEN,
+#                             token_secret: YOUR_TOKEN_SECRET
+#                           })
