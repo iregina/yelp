da73c8679b08ec5fe75cc83e6b99a794dddc74fa
iregina <reginawong14@gmail.com>
2016-03-21 14:19:45 -0700

    add radio buttons to let users pick distnace preferences

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
+
 class HomeController < ApplicationController
   def index
   end
@@ -17,12 +18,29 @@ class HomeController < ApplicationController
 
   def biz
   	address = params[:address]
-    parameters = {category_filter: 'restaurants', radius_filter: 1000}
+    parameters = {category_filter: 'restaurants', radius_filter: params[:radius_filter]}
+
     @businesses = $client.search(address, parameters).businesses.select { |business| business.rating > 4 }
+    @business = pick_random_business(@businesses) 
    	render :summary
   end
 
   def summary
+  end
+
+  private
+
+
+  # This method picks a random business
+  def random_number(businesses)
+  	number_of_businesses = businesses.length
+	random_number = rand(number_of_businesses)
+	return random_number
+  end
+
+  def pick_random_business(businesses)
+  	number = random_number(businesses)
+  	return businesses[number]
   end
 
 end
